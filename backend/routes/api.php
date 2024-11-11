5a28d414fd1c5c0d89e27bf79b95daa50f54b6eb
Pauline <pauline1.virault@epitech.eu>
2024-11-11 21:39:56 +0100

    feat: add CRUD routes for users guides and activities

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -22,6 +22,23 @@ use App\Http\Controllers\ActivityController;
 //     return $request->user();
 // });
 
-Route::resource('users', UserController::class);
-Route::resource('guides', GuideController::class);
-Route::resource('guides.activities', ActivityController::class);
+// User Routes
+Route::get('/users', [UserController::class, 'index']);            // GET /users
+Route::post('/users', [UserController::class, 'store']);           // POST /users
+Route::get('/users/{id}', [UserController::class, 'show']);        // GET /users/{id}
+Route::put('/users/{id}', [UserController::class, 'update']);      // PUT /users/{id}
+Route::delete('/users/{id}', [UserController::class, 'destroy']);  // DELETE /users/{id}
+
+// Guide Routes
+Route::get('/guides', [GuideController::class, 'index']);            // GET /guides
+Route::post('/guides', [GuideController::class, 'store']);           // POST /guides
+Route::get('/guides/{id}', [GuideController::class, 'show']);        // GET /guides/{id}
+Route::put('/guides/{id}', [GuideController::class, 'update']);      // PUT /guides/{id}
+Route::delete('/guides/{id}', [GuideController::class, 'destroy']);  // DELETE /guides/{id}
+
+// Activity Routes (Nested under Guide)
+Route::get('/guides/{guideId}/activities', [ActivityController::class, 'index']);                // GET /guides/{guideId}/activities
+Route::post('/guides/{guideId}/activities', [ActivityController::class, 'store']);               // POST /guides/{guideId}/activities
+Route::get('/guides/{guideId}/activities/{activityId}', [ActivityController::class, 'show']);    // GET /guides/{guideId}/activities/{activityId}
+Route::put('/guides/{guideId}/activities/{activityId}', [ActivityController::class, 'update']);  // PUT /guides/{guideId}/activities/{activityId}
+Route::delete('/guides/{guideId}/activities/{activityId}', [ActivityController::class, 'destroy']); // DELETE /guides/{guideId}/activities/{activityId}
