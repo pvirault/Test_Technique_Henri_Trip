4e5e324a1f87c524d21b6f7763251c8e005bec80
Pauline <pauline1.virault@epitech.eu>
2024-11-11 13:33:54 +0100

    feat : define route in the routes/api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\GuideController;
+use App\Http\Controllers\ActivityController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::resource('users', UserController::class);
+Route::resource('guides', GuideController::class);
+Route::resource('guides.activities', ActivityController::class);
